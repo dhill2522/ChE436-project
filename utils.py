74dbe2940461a4f479eb7340e0ffcf1afdd5ec1b
Daniel Hill <dhill2522@gmail.com>
2018-12-10 16:49:40 -0700

    drastic restructure

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_data(data_file='data.csv'):
+def plot_data(data_file='data.csv', show_plots=True):
     data = pd.read_csv(data_file, sep=',')
 
     plt.figure()
@@ -9,16 +9,23 @@ def plot_data(data_file='data.csv'):
     plt.plot(data['# time'], data['I'], label='I')
     plt.plot(data['# time'], data['D'], label='D')
     plt.plot(data['# time'], data['Err'], label='Error')
-    plt.savefig('PID.png')
     plt.legend()
-    plt.show()
+    plt.savefig('PID.png')
+    if show_plots:
+        plt.show()
+    else:
+        plt.clf()
 
     plt.plot(data['# time'], data['box temp'], label='box temp')
     plt.plot(data['# time'], data['outside temp'], label='outside temp')
     plt.plot(data['# time'], data['SP'], label='setpoint')
-    plt.savefig('temperatures.png')
     plt.legend()
-    plt.show()
+    plt.savefig('temperatures.png')
+    if show_plots:
+        plt.show()
+    else:
+        plt.clf()
+
 
 if __name__ == '__main__':
     plot_data()
