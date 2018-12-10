9d94a9ff569a8faf733ae3789a89718fd7c40756
Daniel Hill <dhill2522@gmail.com>
2018-12-10 15:55:39 -0700

    plotting temperatures and PID

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main_loop(run_time, show_plot=True):
 	tc1 = tclab.TCLab()
 	tc1.LED(100)
 	data = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] # Bogus data row added to make concatenation work, never goes anywhere 
-	csv_file_header = 'time,control output,box humidity,box temp,outside humidity,outside temp,heater 1 temp,heater 2 temp,P,I,D'
+	csv_file_header = 'time,control output,box humidity,box temp,outside humidity,outside temp,heater 1 temp,heater 2 temp,P,I,D,SP'
 	start_time = time.time()
     
 	u = 0
@@ -91,7 +91,7 @@ def main_loop(run_time, show_plot=True):
 			print('time: {:.1f}, u: {}, h_in: {}, t_in: {}, h1: {}, h2: {}, h_out: {}, t_out: {}, P: {:.2f}, I: {:.2f}, D: {:.2f}, sp: {}, error:{:.2f}'
          			.format(current_time, u, humid_in, temp_in, tc1.T1, tc1.T2, humid_out, temp_out, P, I, D, u, sp[i], err[i]))
 			data = np.vstack([data, [current_time, u, humid_in,
-                           temp_in, humid_out, temp_out, tc1.T1, tc1.T2, P, I, D]])
+                           temp_in, humid_out, temp_out, tc1.T1, tc1.T2, P, I, D, sp[i]]])
 			np.savetxt('data.csv', data[1:], delimiter=',', header=csv_file_header)
 			if current_time > run_time*60:
 				print('Run finished. Exiting...')
--- utils.py
+++ utils.py
@@ -10,6 +10,13 @@ def plot_data(data_file='data.csv'):
     plt.plot(data['time'], data['I'], label='I')
     plt.plot(data['time'], data['D'], label='D')
     plt.plot(data['time'], data['Err'], label='Error')
+    plt.savefig('PID.png')
+    plt.show()
+
+    plt.plot(data['time'], data['box temp'], label='box temp')
+    plt.plot(data['time'], data['outside temp'], label='outside temp')
+    plt.plot(data['time'], data['SP'], label='setpoint')
+    plt.savefig('temperatures.png')
     plt.show()
 
 if __name__ == '__main__':
