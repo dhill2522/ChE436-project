4d1f5f9025c2b4e94f53ef074f61825dd5ab8e43
Daniel Hill <dhill2522@gmail.com>
2018-12-13 19:26:27 -0700

    initial auto-tuning controller

--- controller.py
+++ controller.py
@@ -13,8 +13,26 @@ class Controller(object):
 
 
     def auto_tune(self):
-        pass
         # Run a step test
+        print('Running a doublet test on the system...')
+        runs.doublet_test(data_file='tuning_step_test.csv', show_plot=False)
+        
         # Fit the FOPDT parameters
+        print('Fitting FOPDT parameters to the data...')
+        sol = opt.optimize_parameters('tuning_step_test.csv')
+        self.K_p = sol['Kp']
+        self.Tau_p = sol['tauP']
+        self.Theta_P = sol['thetaP']
+        
         # Determine the PID tuning parameters
-        # Return PID parameters
+        print('Determining initial PID tuning parameters')
+        tau_c = max(self.Tau_p, 8*self.Theta_P)
+
+        self.K_c = 1/self.K_p * (self.Tau_p + 0.5*self.Theta_P) / (tau_c + self.Theta_P)
+        self.Tau_I = self.Tau_p + 0.5*self.Theta_P
+        self.Tau_D = self.Tau_p*self.Theta_P / (2*self.Tau_p + self.Theta_P)
+        return
+
+    def run(self, run_time):
+        runs.run_controller(run_time, (self.K_c, self.Tau_I, self.Tau_D))
+        return
